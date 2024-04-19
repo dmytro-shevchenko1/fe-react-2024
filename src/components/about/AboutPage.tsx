03b6863ccd642525b2c14f93bf10872d51917fd2
dmytro-shevchenko1 <d.shevchenko.ch1@gmail.com>
2024-04-19 23:56:20 +0300

    Styles fixes

--- src/components/about/AboutPage.tsx
+++ src/components/about/AboutPage.tsx
@@ -10,22 +10,22 @@ function About() {
                     <div className={styles.introFlex}>
                         <div className={styles.introText}>
                             <h1 className={styles.aboutH1}>About me</h1>
-                            <p>
+                            <p className={styles.aboutParagraph}>
                                 Hi! My name is Dmytro Shevchenko and I&apos;m a Junior Frontend Developer. I am already familiar with main
                                 Web Technologies like React, HTML, CSS, JavaScript and Git version control system.
                             </p>
-                            <p>
+                            <p className={styles.aboutParagraph}>
                                 This page was developed during the course{' '}
                                 <a className={styles.link} href="https://www.mastersacademy.education/frontend-react-it">
                                     &apos;Intro to React&apos;
                                 </a>{' '}
                                 from Masters Academy in 2024.
                             </p>
-                            <p>
+                            <p className={styles.aboutParagraph}>
                                 This is a social project from MOCG company where I got an opportunity to work with Frontend mentors and to
                                 create my own small project for the portfolio.
                             </p>
-                            <p>You can contact me via social network and check out my GitHub.</p>
+                            <p className={styles.aboutParagraph}>You can contact me via social network and check out my GitHub.</p>
                         </div>
                         <img className={styles.aboutImage} src={aboutMe} alt="about" />
                     </div>
