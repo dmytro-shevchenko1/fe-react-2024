6a199645542205da0e3e4d8cdd028e3896a91e32
dmytro-shevchenko1 <d.shevchenko.ch1@gmail.com>
2024-04-26 23:15:02 +0300

    icons fix

--- src/components/SocialIcon/SocialIcon.component.tsx
+++ src/components/SocialIcon/SocialIcon.component.tsx
@@ -1,4 +1,6 @@
 import fbIcon from '@/assets/fb.svg';
+import inst from '@/assets/insta.svg';
+import linkedin from '@/assets/linkedin.svg';
 import styles from '@/components/footer/footer.module.css';
 import FooterIcon from '@/components/footer/FooterIcon.tsx';
 
@@ -6,9 +8,9 @@ function SocialIcon() {
     return (
         <>
             <div className={styles.socialIcons}>
-                <FooterIcon iconPath={fbIcon} link="facebook.com" />
-                <FooterIcon iconPath={fbIcon} link="facebook.com" />
-                <FooterIcon iconPath={fbIcon} link="facebook.com" />
+                <FooterIcon iconPath={fbIcon} link="https://www.facebook.com/profile.php?id=100086849614305" />
+                <FooterIcon iconPath={linkedin} link="https://linkedin.com/" />
+                <FooterIcon iconPath={inst} link="https://www.instagram.com/" />
             </div>
         </>
     );
