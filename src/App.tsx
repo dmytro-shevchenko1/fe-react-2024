0ee58ed7eec44c6ed4dad2d25f812689c8ae3e9c
dmytro-shevchenko1 <d.shevchenko.ch1@gmail.com>
2024-05-04 00:07:59 +0300

    Product List, Switch between About and Product, Cart counter. CRLF to LF

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
+import { useState } from 'react';
+
 import AboutPage from './components/about/AboutPage.component.tsx';
 import FooterComponent from './components/footer/Footer.component.tsx';
 import HeaderComponent from './components/header/Header.component.tsx';
-
-import styles from './App.module.css';
+import ProductsList from './components/productList/ProductList.component.tsx';
 
 function App() {
+    const [currentComponent, setCurrentComponent] = useState<'About' | 'ProductsList'>('About');
+    const [cartCount, setCartCount] = useState<number>(0);
+    const handleChangePage = (component: 'About' | 'ProductsList') => {
+        setCurrentComponent(component);
+    };
+
+    const addToCart = () => {
+        setCartCount(cartCount + 1);
+    };
+
     return (
         <>
-            <HeaderComponent />
-            <main className={styles.body}>
-                <AboutPage />
-            </main>
+            <HeaderComponent handleChangePage={handleChangePage} cartCount={cartCount} />
+            {currentComponent === 'About' ? <AboutPage /> : <ProductsList addToCart={addToCart} />}
             <FooterComponent />
         </>
     );
